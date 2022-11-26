98df328a64672db687ed75e53a54c6e7455abd05
Jorge Ramón <jorge.ramon2128@gmail.com>
2022-11-26 11:56:06 -0600

    Agregando resolvers del primer ejemplo

--- modulo-nodejs/sesion01/02-express-graphql/index.js
+++ modulo-nodejs/sesion01/02-express-graphql/index.js
@@ -6,15 +6,66 @@ const app = express();
 // Tipos disponibles de GraphQL
 // ID, Int, Float, Boolean, String, [T], ENUM
 const schema = buildSchema(`
+  # Definición de los tipos disponibles
   type Product {
     id: ID
     name: String
     price: Float
   }
+
+  # Definición de las consultas disponibles
+  type Query {
+    getAll: [Product]
+    getById(id: ID): Product
+    getByName(name: String): [Product]
+  }
 `);
 
+const products = [
+	{
+		id: 1000,
+		name: "Xbox Series X",
+		price: 15000,
+	},
+	{
+		id: 1001,
+		name: "Nintendo Switch OLED",
+		price: 8000,
+	},
+	{
+		id: 1002,
+		name: "Playstation 5",
+		price: 18000,
+	},
+	{
+		id: 1003,
+		name: "iPhone 14",
+		price: 40000,
+	},
+];
+
+const resolvers = {
+	getAll() {
+		return products;
+	},
+	getById({ id }) {
+		return products.find((p) => p.id === id);
+	},
+	getByName({ name }) {
+		return products.filter((p) =>
+			p.name.toLowerCase().includes(name.toLowerCase())
+		);
+	},
+};
+
 // Integrar GraphQL con Express
-app.use("/graphql", graphqlHTTP({}));
+app.use(
+	"/graphql",
+	graphqlHTTP({
+		schema, // schema: schema,
+		rootValue: resolvers,
+	})
+);
 
 app.listen(8080, function () {
 	console.log("Escuchando puerto 8080");
